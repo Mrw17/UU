1870bb7cc4143f8caaab8aa64f97cfbc329410ca
Daniel <danielwesterlund17@gmail.com>
2020-02-13 15:54:58 +0100

    Updated public long multiplySome(List<Integer> numberList) to work correctly
    Updated public long multiplySome(List<Integer> numberList) to work correctly

--- src/algo/multiply/AlgorithmAndDatastructure.java
+++ src/algo/multiply/AlgorithmAndDatastructure.java
@@ -22,15 +22,29 @@ public class AlgorithmAndDatastructure {
 	 * @return the sum
 	 */
 	public long multiplySome(List<Integer> numberList) {
-		
+
+		int tail = numberList.size();
 		long sum = 0;
-		
-		/* TODO: Write code that multiplies the first element from the beginning
-		 * with the first element from the end, the second element from the 
-		 * beginning with the second element from the end etc. Increase the variable
-		 * sum with the result of each multiplication.  
-		 */				
-			
+
+		/**
+		 * Iterate through the whole list and multiply
+		 * [element0], with [last element].
+		 * [element1], with [last element-1]
+		 * and so on.
+		 */
+		for(int i=0; i < tail; i++) {
+
+			//Goes from left
+			int left = numberList.get(i);
+
+			//goes from right
+			int right = numberList.get(tail-1);
+
+			sum += (left * right);
+
+			//moves one step close to middle from right
+			tail--;
+		}
 		return sum;		
 	}	
 	/**
@@ -43,11 +57,14 @@ public class AlgorithmAndDatastructure {
 	public long multiplyAll(List<Integer> numberList) {
 		long sum = 0;
 
-		/* TODO: Write code that multiplies all elements in the List
-		 * with all the other elements. Increase the variable
-		 * sum with the result of each multiplication.  
-		 */			
-		
-		return sum;		
+		for(int i = 0; i < numberList.size(); i++){
+			int innerLoopSum = 0;
+
+			for(int j = i+1; j < numberList.size(); j++) {
+				innerLoopSum += numberList.get(i) * numberList.get(j);
+			}
+			sum += innerLoopSum;
+		}
+		return sum;
 	}
 }
