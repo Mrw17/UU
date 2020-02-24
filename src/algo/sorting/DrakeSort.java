a6e644f0321b8d9ca1545e718af438d004e2986e
Daniel <danielwesterlund17@gmail.com>
2020-02-24 17:35:02 +0100

    init

---
+++ src/algo/sorting/DrakeSort.java
@@ -0,0 +1,14 @@
+package algo.sorting;
+
+/**
+ * Sort an array containing integers in ascending order
+ * (arranged from smallest to largest) using the Drake sort algorithm.
+ */
+public class DrakeSort implements Sort {
+
+	@Override
+	public int[] sort(int[] unsorted) {
+		// TODO Implement Drake sort
+		return null;
+	}
+}
---
+++ src/algo/sorting/JavaAPISort.java
@@ -0,0 +1,14 @@
+package algo.sorting;
+
+/**
+ * Sort an array containing integers in ascending order
+ * (arranged from smallest to largest) using Java API.
+ */
+public class JavaAPISort implements Sort {
+
+	@Override
+	public int[] sort(int[] unsorted) {
+		// TODO Implement sort using Java API.
+		return null;
+	}
+}
